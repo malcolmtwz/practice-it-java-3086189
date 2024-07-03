e060c1d2e4a589d13b506668f9bd102a58399347
Malcolm Tan <91950295+malcolmtwz@users.noreply.github.com>
2024-07-03 02:45:54 +0000

    Pending changes exported from your codespace

---
+++ _02_02/Tickets.java
@@ -0,0 +1,12 @@
+package _02_02;
+
+public class Tickets {
+
+  private String destination;
+  private double price;
+  private boolean isReturn;
+  public Tickets(){
+
+  }
+  
+}
--- _03_02/GradingSystem.java
+++ _03_02/GradingSystem.java
@@ -4,26 +4,49 @@ public class GradingSystem {
 
   public boolean isAPass(int percentage) {
     // Return true if the percentage is higher than or equal to 60.
+    if(percentage >= 60){
+      return true;
+    }
     // Otherwise return false.
     return false;
   }
 
   public char getGrade(int percentage) {
     // If the percentage is 90 or above, return 'A'.
+    if (percentage >= 90) {
+      return 'A';
+    } else if (percentage >= 80 && percentage <90){
+      return 'B';
+    } else if (percentage >= 70 && percentage <80) {
+      return 'C';
+    } else if (percentage >= 60 && percentage <70) {
+      return 'D';
+    } else {
+      return 'F';
+    }
     // If it's 80-89, return 'B'.
     // If it's 70-79, return 'C'.
     // If it's 60-69, return 'D'.
     // If it's less than 60, return 'F'.
-    return 'X';
   }
 
   public String retakeMessage(int percentage, boolean allowedToRetake) {
     // If percentage is less than 60 and allowedToRetake is true, return a String
+    if (percentage < 60 && allowedToRetake == true){
+      return "The student has been entered for a retake.";
+    }
     // that says "The student has been entered for a retake."
+    
     // If percentage is less than 60 and allowedToRetake is false, return a String
     // that says "The student is not allowed to retake this exam."
+    if (percentage < 60 && allowedToRetake == false) {
+      return "The student is not allowed to retake this exam.";
+    }
     // If percentage is 60 or higher, return a String that says "A retake is not
     // required."
+    if (percentage >= 60){
+      return "A retake is not required.";
+    } 
     return "";
   }
 
